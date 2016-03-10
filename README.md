597c94d58c3adbbefc5912a59676b15689b8d356
Rafael Rezende <rafaelrezend@gmail.com>
2016-03-10 18:28:33 +0100

    Changing felix to equinox.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # jenkins-osgi-sample
+
+## *HelloWorldService* Project
+
+Implements a simple OSGi service.
+It's a Maven project that generates the OSGi Manifest using the `maven-bundle-plugin` plugin.
+
+## *HelloWorld* Project
+
+Uses the OSGi service from *HelloWorldService*.
+As a Maven project, it lists the HelloWorldService as one of its dependencies. Plus, it uses the `<Embed-Dependency>` in order to package the project and all its dependencies into a single *jar*.
--- com.rafaelrezend.HelloWorld/src/main/java/com/rafaelrezend/helloworld/Activator.java
+++ com.rafaelrezend.HelloWorld/src/main/java/com/rafaelrezend/helloworld/Activator.java
@@ -14,7 +14,8 @@ public class Activator implements BundleActivator {
    * (non-Javadoc)
    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
    */
-  public void start(BundleContext context) throws Exception {
+  @Override
+  public void start(final BundleContext context) throws Exception {
     System.out.println("Hello World!!");
     helloWorldServiceReference = context.getServiceReference(HelloWorldService.class.getName());
     HelloWorldService helloWorldService = (HelloWorldService) context.getService(helloWorldServiceReference);
@@ -25,9 +26,12 @@ public class Activator implements BundleActivator {
    * (non-Javadoc)
    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
    */
-  public void stop(BundleContext context) throws Exception {
+  @Override
+  public void stop(final BundleContext context) throws Exception {
     System.out.println("Goodbye World!!");
     context.ungetService(helloWorldServiceReference);
   }
+  
+  
 
 }
